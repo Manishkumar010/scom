9ec68fe526406ef95bcdbdaf598fc7b6a41db9de
Manishkumar010 <mk519956@gmail.com>
2025-06-02 22:32:38 +0530

    earth commit

--- send_email.php
+++ send_email.php
@@ -9,6 +9,7 @@ require 'PHPMailer/src/SMTP.php';
 $Name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
+$number = $_POST['number'];
 
 $mail = new PHPMailer(true);
 
@@ -16,18 +17,19 @@ try {
     $mail->isSMTP();                                    
     $mail->Host       = 'smtp.gmail.com';               
     $mail->SMTPAuth   = true;                           
-    $mail->Username   = 'mk519956@gmail.com';         
-    $mail->Password   = 'llay vwib uatf bsqn';            
+    $mail->Username   = 'scomexperiences@gmail.com';         
+    $mail->Password   = 'wncc hurs ajel eupa';            
     $mail->SMTPSecure = 'tls';                          
     $mail->Port       = 587;                            
 
-    $mail->setFrom('mk519956@gmail.com', 'Website');
-    $mail->addAddress('mk519956@gmail.com');     
+    $mail->setFrom('scomexperiences@gmail.com', 'Website');
+    $mail->addAddress('scomexperiences@gmail.com');     
 
     $mail->isHTML(true);                                
     $mail->Subject = 'Contact Form Submission';
-    $mail->Body    = "<strong>First Name:</strong> $Name  <br>
-                      <strong>Email:</strong> $email <br>
+    $mail->Body    = "<strong>First Name:</strong>$Name <br>
+                      <strong>Email:</strong> $email<br>
+                      <strong>Email:</strong> $number<br>
                       <strong>Message:</strong>$message<br>";
 
     $mail->send();
